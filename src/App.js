17de7776f286af87ceb04c1cd57ad28fec1753d0
Google Jr <andilembele020@gmail.com>
2021-01-27 21:16:46 +0200

    changes to the DiagnosisInput code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import firebase from "./firebase";
 import { DiagnosisInput } from "./components/DiagnosisInput";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Container";
+import "@fontsource/roboto";
 
 function App() {
   // userDiagnosis state
@@ -19,14 +21,43 @@ function App() {
   }, []);
 
   return (
-    <Container>
+    <Container fixed>
       <Button variant="contained" color="primary">
         Hello World
       </Button>
       <div className="App">
+        <header className="App-header">
+          <Typography variant="h1">
+            Welcome to Victoria Falls Municipality
+          </Typography>
+        </header>
         <ul>
           {diagnosis.map((diagnose) => (
             <li key={diagnose.phone}>
+              <DiagnosisInput diagnose={diagnose} />
+            </li>
+          ))}
+        </ul>
+
+        <ul>
+          {diagnosis.map((diagnose) => (
+            <li key={diagnose.symptoms}>
+              <DiagnosisInput diagnose={diagnose} />
+            </li>
+          ))}
+        </ul>
+
+        <ul>
+          {diagnosis.map((diagnose) => (
+            <li key={diagnose.time}>
+              <DiagnosisInput diagnose={diagnose} />
+            </li>
+          ))}
+        </ul>
+
+        <ul>
+          {diagnosis.map((diagnose) => (
+            <li key={diagnose.ageRange}>
               <DiagnosisInput diagnose={diagnose} />
             </li>
           ))}
--- src/components/DiagnosisInput.js
+++ src/components/DiagnosisInput.js
@@ -3,12 +3,16 @@ import firebase from "../firebase";
 
 export const DiagnosisInput = ({ diagnose }) => {
   const [phone, setPhone] = React.useState(diagnose.phone);
+  const [symptoms, setSymptoms] = React.useState(diagnose.symptoms);
+  const [time, setTime] = React.useState(diagnose.phone);
+  const [ageRange, setAgeRange] = React.useState(diagnose.ageRange);
+  const [gender, setGender] = React.useState(diagnose.gender);
 
   const onUpdate = () => {
     const db = firebase.firestore();
     db.collection("userDiagnosis")
       .doc(diagnose.id)
-      .set({ ...diagnose, phone });
+      .set({ ...diagnose, phone, symptoms, time, ageRange, gender });
   };
 
   const onDelete = () => {
@@ -22,6 +26,30 @@ export const DiagnosisInput = ({ diagnose }) => {
         value={phone}
         onChange={(e) => {
           setPhone(e.target.value);
+        }}
+      />
+      <input
+        value={gender}
+        onChange={(e) => {
+          setGender(e.target.value);
+        }}
+      />
+      <input
+        value={symptoms}
+        onChange={(e) => {
+          setSymptoms(e.target.value);
+        }}
+      />
+      <input
+        value={time}
+        onChange={(e) => {
+          setTime(e.target.value);
+        }}
+      />
+      <input
+        value={ageRange}
+        onChange={(e) => {
+          setAgeRange(e.target.value);
         }}
       />
       <button onClick={onUpdate}>Update</button>
