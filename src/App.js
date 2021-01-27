9dcaf48fdab7f8ec7b174fca7d9bde1585380256
Google Jr <andilembele020@gmail.com>
2021-01-28 00:05:47 +0200

    done many things

--- src/App.js
+++ src/App.js
@@ -14,6 +14,37 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import "@fontsource/roboto";
 
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import { orange } from "@material-ui/core/colors";
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B, #FF8E53)",
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: "white",
+    padding: "5px 30px",
+  },
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    },
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Buttons</Button>;
+}
+
 function App() {
   // userDiagnosis state
   const [diagnosis, setDiagnosis] = React.useState([]);
@@ -29,39 +60,42 @@ function App() {
 
   return (
     <Container maxWidth="xd">
-      <div className="App">
-        <header className="App-header">
-          <Typography variant="h1">
-            Welcome to Victoria Falls Municipality
-          </Typography>
-          <Typography variant="subtitle1">
-            Something Interesting goes here
-          </Typography>
-        </header>
-        <AppBar maxWidth="xs" color="secondary">
-          <Toolbar>
-            <IconButton>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h2">Victoria Falls Mascot</Typography>
-            <Button variant="small" color="secondary">
-              Home
-            </Button>
-            <Button variant="small" color="secondary">
-              Login
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <Table>
-          <ol>
-            {diagnosis.map((diagnose) => (
-              <li key={diagnose.id}>
-                <DiagnosisInput diagnose={diagnose} />
-              </li>
-            ))}
-          </ol>
-        </Table>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <Typography variant="h6">
+              Welcome to Victoria Falls Municipality
+            </Typography>
+            <Typography variant="subtitle1">
+              Something Interesting goes here
+            </Typography>
+            <AppBar maxWidth="xs" color="secondary">
+              <Toolbar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h2">
+                  Victoria Falls Mascot Admin Panel
+                </Typography>
+                <Button variant="small" color="secondary">
+                  Home
+                </Button>
+                <Button variant="small" color="secondary">
+                  Login
+                </Button>
+              </Toolbar>
+            </AppBar>
+
+            <ol>
+              {diagnosis.map((diagnose) => (
+                <li key={diagnose.id}>
+                  <DiagnosisInput diagnose={diagnose} />
+                </li>
+              ))}
+            </ol>
+          </header>
+        </div>
+      </ThemeProvider>
     </Container>
   );
 }
