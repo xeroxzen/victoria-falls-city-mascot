5b79bf4e86cf8178c3d03db193010f410e5667d7
Google Jr <andilembele020@gmail.com>
2021-01-29 15:32:45 +0200

    Added ComplaintsInput.js

---
+++ src/components/ComplaintsInput.js
@@ -0,0 +1,61 @@
+import React from "react";
+import firebase from "../firebase";
+import Button from "@material-ui/core/Button";
+// import { Table, TableHead, TableBody, TableRow } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SaveIcon from "@material-ui/icons/Save";
+
+export const ComplaintsInput = ({ data }) => {
+  const [time, setTime] = React.useState(data.time);
+  const [complaint, setComplaint] = React.useState(data.complaint);
+
+  const onUpdate = () => {
+    const db = firebase.firestore();
+    db.collection("Complaints")
+      .doc(data.id)
+      .set({ ...data, time, complaint });
+  };
+
+  const onDelete = () => {
+    const db = firebase.firestore();
+    db.collection("Complaints").doc(data.id).delete();
+  };
+
+  return (
+    <>
+      <input
+        value={complaint}
+        onChange={(e) => {
+          setComplaint(e.target.value);
+        }}
+      />
+      <input
+        value={time}
+        onChange={(e) => {
+          setTime(e.target.value);
+        }}
+      />
+
+      <Button
+        variant="contained"
+        size="small"
+        color="primary"
+        endIcon={<SaveIcon />}
+        spacing="2"
+        onClick={onUpdate}
+      >
+        Update
+      </Button>
+      <Button
+        variant="disabled"
+        size="small"
+        color="secondary"
+        endIcon={<DeleteIcon />}
+        spacing="2"
+        onClick={onDelete}
+      >
+        Delete
+      </Button>
+    </>
+  );
+};
