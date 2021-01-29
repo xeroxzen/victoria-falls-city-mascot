d7f724650eceaeb0c89bda2e46deea2d6e43f54e
Google Jr <andilembele020@gmail.com>
2021-01-29 15:59:27 +0200

    Added RecommendationInput.js

---
+++ src/components/RecommendationInput.js
@@ -0,0 +1,60 @@
+import React from "react";
+import firebase from "../firebase";
+import Button from "@material-ui/core/Button";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SaveIcon from "@material-ui/icons/Save";
+
+export const RecommendationInput = ({ data }) => {
+  const [date, setDate] = React.useState(data.date);
+  const [recommendation, setRecommendation] = React.useState(data.symptoms);
+
+  const onUpdate = () => {
+    const db = firebase.firestore();
+    db.collection("Recommendation")
+      .doc(date.id)
+      .set({ ...data, date, recommendation });
+  };
+
+  const onDelete = () => {
+    const db = firebase.firestore();
+    db.collection("userDiagnosis").doc(data.id).delete();
+  };
+
+  return (
+    <>
+      <input
+        value={date}
+        onChange={(e) => {
+          setDate(e.target.value);
+        }}
+      />
+      <input
+        value={recommendation}
+        onChange={(e) => {
+          setRecommendation(e.target.value);
+        }}
+      />
+
+      <Button
+        variant="contained"
+        size="small"
+        color="primary"
+        endIcon={<SaveIcon />}
+        spacing="2"
+        onClick={onUpdate}
+      >
+        Update
+      </Button>
+      <Button
+        variant="disabled"
+        size="small"
+        color="secondary"
+        endIcon={<DeleteIcon />}
+        spacing="2"
+        onClick={onDelete}
+      >
+        Delete
+      </Button>
+    </>
+  );
+};
